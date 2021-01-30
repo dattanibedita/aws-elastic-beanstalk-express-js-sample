fd2f4c78d0f19234614a97862f1af44df4a9f945
dattanibedita <dattanibedita@gmail.com>
2021-01-30 19:10:01 +0530

    changed app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Running continuous delivery pipeline using AWS - Nibedita Datta'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
